d713291dc0fe750ff0150a581d7d90abc3afdccf
sqvix2yes <165594434+sqvix2yes@users.noreply.github.com>
2024-03-31 21:51:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to PrivateHeberg!","Watching 24/7!"];
 
 
 let currentIndex = 0;
